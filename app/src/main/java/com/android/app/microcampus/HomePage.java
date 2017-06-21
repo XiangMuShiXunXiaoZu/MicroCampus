8981c36fc7a3ff76574bc0a7bc054243df57d524
lulujie <l.roger1996@gmail.com>
2017-06-21 10:22:02 +0800

    add item activity

--- app/src/main/java/com/android/app/microcampus/HomePage.java
+++ app/src/main/java/com/android/app/microcampus/HomePage.java
@@ -1,7 +1,9 @@
 package com.android.app.microcampus;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class HomePage extends Fragment {
 
+    private FloatingActionButton fab;
 
     public HomePage() {
         // Required empty public constructor
@@ -22,8 +25,22 @@ public class HomePage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page,container,false);
+        FloatingActionButton fab = (FloatingActionButton)view.findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(),ItemActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        return view;
     }
+
 
 }
