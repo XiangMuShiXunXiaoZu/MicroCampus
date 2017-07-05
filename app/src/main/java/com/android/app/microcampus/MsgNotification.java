0280c4f712fe522ca1203890f16a6bf3a55128f4
lulujie <l.roger1996@gmail.com>
2017-07-05 08:38:34 +0800

    Merge branch 'develop' of https://github.com/XiangMuShiXunXiaoZu/MicroCampus into develop
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/android/app/microcampus/MsgNotification.java
+++ app/src/main/java/com/android/app/microcampus/MsgNotification.java
@@ -3,13 +3,24 @@ package com.android.app.microcampus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,12 +34,20 @@ public class MsgNotification extends Fragment {
     private SimpleAdapter myAdapter;
     private ArrayList<HashMap<String, Object>> listmsg = new ArrayList<HashMap<String, Object>>();
     private ListView listView;
+    private static final String url="http://123.206.125.253/checkmessage"; //所需url
+    private Data app;
+
 
 
     public MsgNotification() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        app = (Data)getActivity().getApplication();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,11 +56,36 @@ public class MsgNotification extends Fragment {
         View view = inflater.inflate(R.layout.fragment_msg_notification, container, false);
 
         listView = (ListView)view.findViewById(R.id.listView);
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        map.put("image", R.drawable.ic_home_black_24dp);
-        map.put("username", "一四又三");
-        map.put("message", "陈树：淡定");
-        listmsg.add(map);
+//        HashMap<String, Object> map = new HashMap<String, Object>();
+//        map.put("image", R.drawable.ic_home_black_24dp);
+//        map.put("username", "一四又三");
+//        map.put("message", "陈树：淡定");
+//        listmsg.add(map);
+
+        HashMap<Object, Object>userMap=new HashMap<Object, Object>();
+        userMap.put("userId", app.getUserId());
+        JSONObject map=new JSONObject(userMap);
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, map, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try{
+                int userId = response.getInt("userId");
+
+                }catch (JSONException e){
+                    Log.e("ERROR", e.getMessage(), e);
+                    Toast.makeText(getContext(), "服务器返回参数有误", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }},new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        );
 
         myAdapter = new SimpleAdapter(getContext(),
                 listmsg,
